1a5fee5385061dd764104a8874c44581d0beb3d8
Sagar Adhikari <sagaradhikaricode@gmail.com>
2025-11-16 20:21:19 +0545

    Initial commit

--- App.tsx
+++ App.tsx
@@ -5,41 +5,70 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import React, {useState} from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+   StatusBar,
+   StyleSheet,
+   View,
+   Text,
+   TouchableOpacity
+  } from 'react-native';
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+ const [randomBackground, setRandomBackground] = useState("#ffffff");
+
+ const generateColor = () => {
+  const hexRange = '0123456789ABCDEF';
+  let color = '#';
+
+  for(let i=0; i<6; i++){
+    color += hexRange[Math.floor(Math.random() * 16)]
+
+    setRandomBackground(color);
+ }
+}
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+   <>
+   <StatusBar backgroundColor={randomBackground}/>
+   <View style={[styles.container , {backgroundColor: randomBackground}] }>
+    <TouchableOpacity  onPress={generateColor}>
+      <View style={styles.actionButton}>
+        <Text style={styles.actionButtonTxt}>Press Me</Text>
+      </View>
+    </TouchableOpacity>
+
+   </View>
+   </>
+      
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent: 'center',
   },
+  actionButton:{
+    borderRadius:12,
+    backgroundColor:'#6A184D',
+    paddingVertical:10,
+    paddingHorizontal:40,
+  },
+  actionButtonTxt:{
+  fontSize:24,
+  color:'#FFFFFF',
+  
+  textTransform:"uppercase",
+  }
+
 });
+
 
 export default App;
